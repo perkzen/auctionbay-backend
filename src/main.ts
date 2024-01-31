8c7c0fbdd1f1cf8457518e209ce847aa54997706
perkzen <perko.domen@gmail.com>
2024-01-31 20:00:15 +0100

    fix: run boostrap async

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+(async (): Promise<void> => {
+  try {
+    await bootstrap();
+  } catch (e) {
+    Logger.error(e, 'Error');
+  }
+})();
