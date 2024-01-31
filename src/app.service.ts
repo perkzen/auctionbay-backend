554476e046197cb0375c511aca62785ba272c078
perkzen <perko.domen@gmail.com>
2024-01-31 19:55:27 +0100

    feat: add health check

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    healthCheck() {
+        return {
+            status: 'ok',
+            uptime: process.uptime(),
+            timestamp: Date.now(),
+        };
+    }
 }
