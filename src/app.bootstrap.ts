4fad0a09e27b7b1bc75847e14c6b729197987da4
perkzen <perko.domen@gmail.com>
2024-02-02 00:07:01 +0100

    feat: add logging.interceptor.ts

--- src/app.bootstrap.ts
+++ src/app.bootstrap.ts
@@ -1,10 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
-import compression from 'compression';
+import * as compression from 'compression';
 import settings from './app.settings';
 import { ExceptionFilter } from './common/filters/exception.filter';
 import { HttpAdapterHost } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 export const bootstrapSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
@@ -18,24 +19,23 @@ export const bootstrapSwagger = (app: INestApplication) => {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(settings.app.swaggerPath, app, document);
-
-  return app;
 };
 
 export const bootstrapGlobalPipe = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  return app;
 };
 
 export const bootstrapGlobalFilters = (app: INestApplication) => {
   const adapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new ExceptionFilter(adapterHost));
-  return app;
+};
+
+export const bootstrapGlobalInterceptors = (app: INestApplication) => {
+  app.useGlobalInterceptors(new LoggingInterceptor());
 };
 
 export const bootstrapMiddlewares = (app: INestApplication) => {
   app.use(compression());
   app.use(helmet());
   app.enableCors({ origin: settings.app.corsOrigin });
-  return app;
 };
