c2867deffed086df3cc83d69fda24da0bf18b6ea
perkzen <perko.domen@gmail.com>
2024-02-01 23:40:46 +0100

    feat: add cors, helmet, compression

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import settings from './app.settings';
-import { bootstrapGlobalPipe, bootstrapSwagger } from './app.bootstrap';
+import {
+  bootstrapGlobalPipe,
+  bootstrapMiddlewares,
+  bootstrapSwagger,
+} from './app.bootstrap';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   bootstrapSwagger(app);
   bootstrapGlobalPipe(app);
+  bootstrapMiddlewares(app);
 
   await app.listen(settings.app.port);
   Logger.log(
