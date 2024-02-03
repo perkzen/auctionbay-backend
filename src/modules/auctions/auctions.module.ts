34140599edcf1967cb8a62459cfb38961e99c2b7
perkzen <perko.domen@gmail.com>
2024-02-03 20:27:29 +0100

    fix: imports

--- src/modules/auctions/auctions.module.ts
+++ src/modules/auctions/auctions.module.ts
@@ -1,9 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuctionsController } from './auctions.controller';
 import { AuctionsService } from './auctions.service';
 import { AuctionsJob } from './auctions.job';
-import { NotificationsGateway } from '../notifications/notifications.gateway';
-import { AuthModule } from '../auth/auth.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
