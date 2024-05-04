e1dbdfc39ce4fb2338d04f011a4af87630d60970
perkzen <perko.domen@gmail.com>
2024-05-04 16:26:47 +0200

    fix: update auction

--- src/modules/auctions/dtos/update-auction.dto.ts
+++ src/modules/auctions/dtos/update-auction.dto.ts
@@ -1,13 +1,14 @@
-import { CreateAuctionDTO } from './create-auction.dto';
-import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
-import { AuctionStatus } from '@prisma/client';
 
-export class UpdateAuctionDTO extends PartialType(CreateAuctionDTO) {
-  @ApiProperty({ required: false })
-  @IsEnum(AuctionStatus, {
-    message: 'Invalid status',
-  })
-  status?: AuctionStatus;
+export class UpdateAuctionDTO {
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  endsAt: Date;
+
+  imageUrl?: string;
 }
