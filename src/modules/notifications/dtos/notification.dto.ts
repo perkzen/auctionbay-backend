b7e38a3aad1616fd9d29ce36557bc9d56d495454
perkzen <perko.domen@gmail.com>
2024-05-21 22:44:20 +0200

    fix: dto

--- src/modules/notifications/dtos/notification.dto.ts
+++ src/modules/notifications/dtos/notification.dto.ts
@@ -1,16 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { JsonValue } from '@prisma/client/runtime/library';
+import { IsDateString, IsJSON, IsString } from 'class-validator';
+import { Expose } from 'class-transformer';
 
 export class NotificationDTO {
   @ApiProperty()
+  @IsString()
+  @Expose()
   id: string;
 
   @ApiProperty()
+  @IsJSON()
+  @Expose()
   data: JsonValue;
 
   @ApiProperty()
+  @IsString()
+  @Expose()
   userId: string;
 
   @ApiProperty()
+  @IsDateString()
+  @Expose()
   createdAt: Date;
 }
